329888a7e7f18809559058fd5c580a36dc0982f5
Felix Zhu <fzhu@atlassian.com>
2022-12-26 22:45:47 +1100

    .

--- ConfluenceHelper/main.py
+++ ConfluenceHelper/main.py
@@ -1,2 +1,90 @@
-class Confluence:
-    ...
+from atlassian import Confluence
+import base64
+import requests
+from jinja2 import Template
+import logging
+import json
+
+
+class ConfluenceGenerator(Confluence): 
+    def __init__(self, base_url, username, api_token):
+        self.base_url = base_url
+        self.username = username
+        self.api_token = api_token
+        Confluence.__init__(
+            self,
+            url=self.base_url,
+            username=self.username,
+            password=self.api_token,
+            cloud=True,
+        )
+        
+    def create_or_update_page(
+        self,
+        title: str,
+        parent_id: str,
+        overwrite: bool = False,
+        representation="storage",
+    ):
+        """Create or update a Confluence page as the experiment report
+
+        Args:
+            title (str): Title of the Confluence page
+            parent_id (str): ID of the parent Confluence page
+            overwrite (bool): Whether to overwrite existing pages
+            representation (str, optional): Representation of the Confluence page. Defaults to "storage".
+        """
+
+        space = self.get_page_space(parent_id)
+
+        # Logger disable sandwich because of annoying logged error message if page doesn't exist.
+        logging.getLogger("atlassian.confluence").disabled = True
+        logging.getLogger("atlassian.rest_client").disabled = True
+        page_exists = self.page_exists(space, title)
+        logging.getLogger("atlassian.confluence").disabled = False
+        logging.getLogger("atlassian.rest_client").disabled = False
+
+        if page_exists:
+            if not overwrite:
+                raise ValueError(
+                    f"{title} already exists in space {space}. Set overwrite to True to update."
+                )
+            page_id = self.get_page_id(space, title)
+            result = self.update_page(
+                parent_id=parent_id,
+                page_id=page_id,
+                title=title,
+                body=self.xml,
+                representation=representation,
+            )
+
+        else:
+            result = self.create_page(
+                space=space,
+                parent_id=parent_id,
+                title=title,
+                body=self.xml,
+                representation=representation,
+                editor="v2",
+            )
+            page_id = result["id"]
+
+        self.attach_plotly_plots(page_id=page_id, space=space, title=title)
+
+        print(
+            f"Your Confluence page has been uploaded at {self.base_url}/wiki/spaces/{result['space']['key']}/pages/{result['id']}"
+        )
+        return
+
+    def get_page_body(self, page_id):
+        auth = self.confluence_username + ":" + self.confluence_api_token
+        encoded_auth = base64.b64encode(auth.encode()).decode()
+        url = f"https://hello.atlassian.net/wiki/rest/api/content/{page_id}"
+        headers = {
+            "Accept": "application/json",
+            "Authorization": f"Basic {encoded_auth}",
+        }
+        response = requests.get(
+            url, headers=headers, params={"expand": ["body.storage"]}
+        ).json()
+        return response["body"]["storage"]["value"]
