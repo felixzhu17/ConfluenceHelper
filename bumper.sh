7c971058e8cd76c2021163a0bb847d7a7ec6b9ad
Felix Zhu <fzhu@atlassian.com>
2022-12-28 23:38:19 +1100

    Bump version: 0.0.8 → 0.0.9

--- bumper.sh
+++ bumper.sh
@@ -1,6 +1,6 @@
 black . 
 git commit -a -m 'minor update'
-bumpversion --verbose --current-version 0.0.8 patch
+bumpversion --verbose --current-version 0.0.9 patch
 git push
 bash update_package.sh
 
