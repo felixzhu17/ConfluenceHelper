e8be7b3a6b30e1a170573ae31e74e6666e2ad5f9
Felix Zhu <fzhu@atlassian.com>
2022-12-28 23:59:27 +1100

    Bump version: 0.0.13 → 0.0.14

--- bumper.sh
+++ bumper.sh
@@ -1,6 +1,6 @@
 black . 
 git commit -a -m 'minor update'
-bumpversion --verbose --current-version 0.0.13 patch
+bumpversion --verbose --current-version 0.0.14 patch
 git push
 bash update_package.sh
 
--- update_package.sh
+++ update_package.sh
@@ -1,2 +1,2 @@
 python setup.py sdist bdist_wheel --universal
-twine upload --repository pypi dist/confluence_generator-0.0.13-py2.py3-none-any.whl
+twine upload --repository pypi dist/confluence_generator-0.0.14-py2.py3-none-any.whl
