b2ca1bcb06fba247b14fa916fc15f4b25f5ef253
Felix Zhu <fzhu@atlassian.com>
2022-12-28 23:14:39 +1100

    Bump version: 0.0.1 → 0.0.2

--- update_package.sh
+++ update_package.sh
@@ -1,2 +1,2 @@
 python setup.py sdist bdist_wheel --universal
-twine upload --repository pypi dist/confluence_generator-0.0.1-py2.py3-none-any.whl
+twine upload --repository pypi dist/confluence_generator-0.0.2-py2.py3-none-any.whl
